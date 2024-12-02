d9f9f7806609b2c914cd7db3f3464b91c6df31b3
Linus A Back <linus.a.back@gmail.com>
2024-12-02 21:44:47 +0100

    solved day 2 part 2

--- internal/year2024/day2/solve.go
+++ internal/year2024/day2/solve.go
@@ -12,71 +12,91 @@ const (
 
 func Solve() (solution1, solution2 string, err error) {
 	var (
-		reports  [][]int64
-		reports2 [][]int64
-		report   []int64
+		//reports  [][]int64
+		//reports2 [][]int64
+		report       []int64
+		safe1, safe2 int
 	)
-	err = util.DoEachRowFile(exampleFile, func(row []byte, nr int) error {
+
+	err = util.DoEachRowFile(inputFile, func(row []byte, nr int) error {
 		report = util.ParseInt64ArrNoError(row)
-		reports = appendIfSafe(reports, report, isSafe)
-		reports2 = appendIfSafe(reports2, report, isSafe2)
+		if len(report) == 0 {
+			return nil
+		}
+		if isSafe(report) {
+			safe1++
+			safe2++
+			return nil
+		}
+
+		if isSafe2(report) {
+			safe2++
+		}
+		//reports = appendIfSafe(reports, report, isSafe)
+		//reports2 = appendIfSafe(reports2, report, isSafe2)
+
 		return nil
 	})
 	if err != nil {
 		return
 	}
 
-	solution1 = strconv.Itoa(len(reports))
+	solution1 = strconv.Itoa(safe1)
+	//fmt.Println(reports2)
+	solution2 = strconv.Itoa(safe2)
 	return
 }
 
 func isSafe2(report []int64) bool {
-	var isIncrease bool
-	diff := report[1] - report[0]
-	if diff == 0 {
-		return false
+	// remove first
+	if isSafe(report[1:]) {
+		//fmt.Println("  safe if remove first")
+		return true
 	}
-	isIncrease = diff > 0
-	if !check(diff, isIncrease) {
-		return false
+
+	// try last
+	if isSafe(report[:len(report)-1]) {
+		//fmt.Println("  safe if remove last")
+		return true
 	}
-	for i, val := range report[2:] {
-		diff = val - report[i+1]
-		if !check(diff, isIncrease) {
-			return false
+
+	alt := make([]int64, len(report)-1)
+	for remove := 1; remove < len(alt); remove++ {
+		copyExcept(alt, report, remove)
+		if isSafe(alt) {
+			//fmt.Printf("  safe if remove %d: %v\n", report[remove], alt)
+			return true
 		}
 	}
-	return true
+
+	return false
+}
+
+func copyExcept(dst, src []int64, except int) {
+	n := copy(dst, src[:except])
+	copy(dst[n:], src[except+1:])
 }
 
 func isSafe(report []int64) bool {
-	var isIncrease bool
 	diff := report[1] - report[0]
 	if diff == 0 {
 		return false
 	}
-	isIncrease = diff > 0
-	if !check(diff, isIncrease) {
-		return false
+	var (
+		lower int64 = 1
+		upper int64 = 3
+	)
+	if diff < 0 {
+		lower = -3
+		upper = -1
 	}
-	for i, val := range report[2:] {
-		diff = val - report[i+1]
-		if !check(diff, isIncrease) {
+	for i, val := range report[1:] {
+		diff = val - report[i]
+		if lower > diff || diff > upper {
 			return false
 		}
 	}
 	return true
-}
-
-func check(diff int64, isIncrease bool) bool {
-	if !isIncrease {
-		diff *= -1
-	}
-	return checkIncrease(diff)
-}
-
-func checkIncrease(diff int64) bool {
-	return 1 <= diff && diff <= 3
 }
 
 func appendIfSafe(reports [][]int64, report []int64, safeFunc func(report []int64) bool) [][]int64 {
