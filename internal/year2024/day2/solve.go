bd0afe39bc4e5ffe2ea98b652ed5174da9278947
Linus A Back <linus.a.back@gmail.com>
2024-12-02 19:15:28 +0100

    solved day2 part 1

--- internal/year2024/day2/solve.go
+++ internal/year2024/day2/solve.go
@@ -1,8 +1,8 @@
 package day2
 
 import (
-	"log"
-	"os"
+	"github.com/linusback/aoc2024/pkg/util"
+	"strconv"
 )
 
 const (
@@ -11,11 +11,77 @@ const (
 )
 
 func Solve() (solution1, solution2 string, err error) {
-	log.Println("welcome to day 2 of advent of code")
-	b, err := os.ReadFile(exampleFile)
+	var (
+		reports  [][]int64
+		reports2 [][]int64
+		report   []int64
+	)
+	err = util.DoEachRowFile(exampleFile, func(row []byte, nr int) error {
+		report = util.ParseInt64ArrNoError(row)
+		reports = appendIfSafe(reports, report, isSafe)
+		reports2 = appendIfSafe(reports2, report, isSafe2)
+		return nil
+	})
 	if err != nil {
 		return
 	}
-	log.Println(string(b))
+
+	solution1 = strconv.Itoa(len(reports))
 	return
+}
+
+func isSafe2(report []int64) bool {
+	var isIncrease bool
+	diff := report[1] - report[0]
+	if diff == 0 {
+		return false
+	}
+	isIncrease = diff > 0
+	if !check(diff, isIncrease) {
+		return false
+	}
+	for i, val := range report[2:] {
+		diff = val - report[i+1]
+		if !check(diff, isIncrease) {
+			return false
+		}
+	}
+	return true
+}
+
+func isSafe(report []int64) bool {
+	var isIncrease bool
+	diff := report[1] - report[0]
+	if diff == 0 {
+		return false
+	}
+	isIncrease = diff > 0
+	if !check(diff, isIncrease) {
+		return false
+	}
+	for i, val := range report[2:] {
+		diff = val - report[i+1]
+		if !check(diff, isIncrease) {
+			return false
+		}
+	}
+	return true
+}
+
+func check(diff int64, isIncrease bool) bool {
+	if !isIncrease {
+		diff *= -1
+	}
+	return checkIncrease(diff)
+}
+
+func checkIncrease(diff int64) bool {
+	return 1 <= diff && diff <= 3
+}
+
+func appendIfSafe(reports [][]int64, report []int64, safeFunc func(report []int64) bool) [][]int64 {
+	if len(report) > 0 && safeFunc(report) {
+		return append(reports, report)
+	}
+	return reports
 }
