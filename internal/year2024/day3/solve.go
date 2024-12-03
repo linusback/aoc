c2fc89522f676814716a1eaba99eef83614de674
Linus A Back <linus.a.back@gmail.com>
2024-12-03 22:55:13 +0100

    Solve day 3 part 2

--- internal/year2024/day3/solve.go
+++ internal/year2024/day3/solve.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 )
 
 const (
-	exampleFile = "./internal/year2024/day3/example"
-	inputFile   = "./internal/year2024/day3/input"
+	exampleFile  = "./internal/year2024/day3/example"
+	exampleFile2 = "./internal/year2024/day3/example2"
+	inputFile    = "./internal/year2024/day3/input"
 )
 
 func Solve() (solution1, solution2 string, err error) {
@@ -23,6 +25,20 @@ func Solve() (solution1, solution2 string, err error) {
 	if err != nil {
 		return
 	}
+	// wrong 29018484
+	//reg2, err := regexp.Compile(`(?m)(don't\(\).*do\(\))|(don't\(\).*)`)
+	//if err != nil {
+	//	return
+	//}
+	//cleaned := reg2.ReplaceAllLiteral(b, nil)
+
+	//fmt.Println("cleaned: ", string(cleaned))
+
+	cleaned, err := cleanBytes(b)
+	if err != nil {
+		return
+	}
+
 	mul := reg.FindAllSubmatch(b, -1)
 	var acc1 int64
 	for _, m := range mul {
@@ -31,7 +47,81 @@ func Solve() (solution1, solution2 string, err error) {
 		}
 		acc1 += util.ParseInt64NoError(m[1]) * util.ParseInt64NoError(m[2])
 	}
+	mul = reg.FindAllSubmatch(cleaned, -1)
+	var acc2 int64
+	for _, m := range mul {
+		if len(m) < 3 {
+			continue
+		}
+		acc2 += util.ParseInt64NoError(m[1]) * util.ParseInt64NoError(m[2])
+	}
+
 	solution1 = strconv.FormatInt(acc1, 10)
+	//fmt.Println(acc2)
+	solution2 = strconv.FormatInt(acc2, 10)
 
 	return
+}
+
+func cleanBytes(src []byte) (res []byte, err error) {
+	startRegex, err := regexp.Compile(`don't\(\)`)
+	if err != nil {
+		return
+	}
+	stopRegex, err := regexp.Compile(`do\(\)`)
+	if err != nil {
+		return
+	}
+	startIdx := startRegex.FindAllIndex(src, -1)
+	stopIdx := stopRegex.FindAllIndex(src, -1)
+
+	startIgnore := toIntArray(startIdx, 0)
+	stopIgnore := toIntArray(stopIdx, 1)
+
+	// just in case
+	slices.Sort(startIgnore)
+	slices.Sort(stopIgnore)
+
+	//fmt.Println(startIgnore)
+	//fmt.Println(stopIgnore)
+	res = make([]byte, 0, len(src))
+	start := 0
+	stop := startIgnore[0]
+	res = append(res, src[start:stop]...)
+	startIgnore = startIgnore[1:]
+
+	for {
+		start, stopIgnore = getBiggerThen(stopIgnore, stop)
+		if start == -1 {
+			break
+		}
+		stop, startIgnore = getBiggerThen(startIgnore, start)
+		if stop == -1 {
+			//fmt.Println(string(src[start:]))
+			res = append(res, src[start:]...)
+			break
+		}
+		//fmt.Println(string(src[start:stop]))
+		res = append(res, src[start:stop]...)
+	}
+	// just in case sort
+
+	return res, nil
+}
+
+func toIntArray(src [][]int, idx int) []int {
+	res := make([]int, len(src))
+	for i, pair := range src {
+		res[i] = pair[idx]
+	}
+	return res
+}
+
+func getBiggerThen(res []int, n int) (int, []int) {
+	for i, i2 := range res {
+		if i2 > n {
+			return i2, res[i+1:]
+		}
+	}
+	return -1, nil
 }
---
+++ pkg/util/tokenizer.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"github.com/linusback/aoc2024/pkg/errorsx"
+	"unsafe"
+)
+
+const (
+	ErrPatternIsNull errorsx.SimpleError = "pattern cannot be null"
+)
+
+type PatternFunc func([]byte) bool
+
+type StringPattern string
+
+func (s StringPattern) Pattern(p []byte) bool {
+	if len(p) < len(s) {
+		return false
+	}
+	return BytesEqualString(p[:len(s)], string(s))
+}
+
+type Patterner interface {
+	Pattern([]byte) bool
+}
+
+type Tokenizer struct {
+	src      []byte
+	patterns []Patterner
+}
+
+func NewTokenizer(src []byte, p Patterner, patterns ...Patterner) (t *Tokenizer, err error) {
+	if p == nil {
+		return nil, ErrPatternIsNull
+	}
+	for _, pe := range patterns {
+		if pe == nil {
+			return nil, ErrPatternIsNull
+		}
+	}
+	t = new(Tokenizer)
+	t.src = src
+
+	return
+}
+
+func BytesEqualString(b []byte, s string) bool {
+	return ToUnsafeString(b) == s
+}
+
+func ToUnsafeString(s []byte) string {
+	return *(*string)(unsafe.Pointer(&s))
+}
