d84b86509c9cb838c0ecefc9085f96aa2645c65b
Linus A Back <linus.a.back@gmail.com>
2024-12-08 05:23:08 +0100

    solved day 7

--- internal/year2024/day7/solve.go
+++ internal/year2024/day7/solve.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"github.com/linusback/aoc/pkg/util"
 	"log"
-	"maps"
-	"slices"
+	"strconv"
 )
 
 const (
@@ -13,7 +12,12 @@ const (
 	inputFile          = "./internal/year2024/day7/input"
 	plus        symbol = '+'
 	multi       symbol = '*'
+	concatenate symbol = '|'
 )
+
+func Solve() (solution1, solution2 string, err error) {
+	return solve(inputFile)
+}
 
 type symbol byte
 
@@ -23,19 +27,23 @@ func (s symbol) apply(a, b uint64) uint64 {
 		return a * b
 	case plus:
 		return a + b
+	case concatenate:
+		str := fmt.Sprintf("%d%d", a, b)
+		res, err := strconv.ParseUint(str, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return res
 	default:
 		log.Fatalf("unknown symbol %c", s)
 		return 0
 	}
 }
 
-func Solve() (solution1, solution2 string, err error) {
-	return solve(inputFile)
-}
-
 func solve(filename string) (solution1, solution2 string, err error) {
 	var rules [][]uint64
 	ruleLen := make(map[int]int)
+
 	err = util.DoEachRowFile(filename, func(row []byte, nr int) error {
 		rule := util.ParseUint64ArrNoError(row)
 		rules = append(rules, rule)
@@ -48,25 +56,42 @@ func solve(filename string) (solution1, solution2 string, err error) {
 	if err != nil {
 		return
 	}
-	var acc1 uint64
+
+	var acc1, acc2 uint64
 	for _, r := range rules {
-		if isValid1(r) {
+		if isValid(r, plus, multi) {
+			//log.Println(r)
 			acc1 += r[0]
+			acc2 += r[0]
+		} else if isValid(r, plus, multi, concatenate) {
+			acc2 += r[0]
 		}
 	}
-	keys := slices.Collect(maps.Keys(ruleLen))
-	slices.Sort(keys)
-	for _, k := range keys {
-		fmt.Println("len: ", k, ", count: ", ruleLen[k])
-	}
+
+	solution1 = strconv.FormatUint(acc1, 10)
+	solution2 = strconv.FormatUint(acc2, 10)
 
 	return
 }
 
-func isValid1(r []uint64) bool {
-	//res := r[0]
-	//acc := r[1]
-	//rest := r[2:]
+func isValid(r []uint64, symbols ...symbol) bool {
+	//fmt.Println(r)
+	res := r[0]
+	acc := r[1]
+	rest := r[2:]
 
+	for o := range util.Combinate(len(rest), symbols...) {
+		for i, s := range o {
+			acc = s.apply(acc, rest[i])
+			//fmt.Println("acc is: ", acc)
+			if acc > res {
+				break
+			}
+		}
+		if acc == res {
+			return true
+		}
+		acc = r[1]
+	}
 	return false
 }
