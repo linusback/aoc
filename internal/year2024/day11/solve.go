fd64eb5469e623a26091c0747db9ff29ab5be7d3
Linus A Back <linus.a.back@gmail.com>
2024-12-17 20:27:44 +0100

    done day 11 part 2

--- internal/year2024/day11/solve.go
+++ internal/year2024/day11/solve.go
@@ -2,7 +2,6 @@ package day11
 
 import (
 	"github.com/linusback/aoc/pkg/util"
-	"log"
 	"os"
 	"strconv"
 )
@@ -17,100 +16,107 @@ func Solve() (solution1, solution2 string, err error) {
 	return solve(inputFile)
 }
 
+const (
+	blinks1 = 25
+	blinks2 = 75
+)
+
+type key struct {
+	stone uint64
+	depth uint8
+}
+
+var cache = map[key]uint64{}
+
 func solve(filename string) (solution1, solution2 string, err error) {
-	const blinks = 40
-	//const blinks = 1
-	// 18 446 744 073 709 551 615
 	b, err := os.ReadFile(filename)
 	if err != nil {
 		return
 	}
 	arr := util.ParseUint64ArrNoError(b)
-	log.Printf("bytes: %s", b)
-	log.Printf("arr: %v", arr)
-	res := arr
-	for i := range blinks {
-		res = blink(res)
-		if i == 24 {
-			solution1 = strconv.Itoa(len(res))
-		}
-		log.Println(i, "total", len(res))
+
+	var total uint64
+	for _, u := range arr {
+		total += countValuesCached(u, key{}, blinks1)
 	}
-	
+
+	solution1 = strconv.FormatUint(total, 10)
+
+	total = 0
+	for _, u := range arr {
+		total += countValuesCached(u, key{}, blinks2)
+	}
+	solution2 = strconv.FormatUint(total, 10)
+
 	return
 }
 
-func blink(arr []uint64) []uint64 {
-	res := make([]uint64, 0, len(arr)*2)
-	for _, u := range arr {
-		res = newValuesAppend(res, u)
+func countValuesCached(u uint64, k key, depth uint8) uint64 {
+	if depth == 0 {
+		return 1
 	}
-	return res
-}
-
-func newValuesAppend(res []uint64, u uint64) []uint64 {
-	var count, div uint64
+	depth--
+	k.stone = u
+	k.depth = depth
+	if cached, ok := cache[k]; ok {
+		return cached
+	}
+	var val uint64
 	if u == 0 {
-		return append(res, 1)
+		val = countValuesCached(1, k, depth)
+	} else if count, div := CountDigitsDivisor2(u); count%2 == 0 {
+		val = countValuesCached(u/div, k, depth) + countValuesCached(u%div, k, depth)
+	} else {
+		val = countValuesCached(u*2024, k, depth)
 	}
-
-	if count, div = CountDigitsDivisor(u); count%2 == 0 {
-		return append(res, u/div, u%div)
-	}
-
-	return append(res, u*2024)
+	cache[k] = val
+	return val
 }
 
-// 52 74
-// 5274 / 100 = 52
-// 5274 % 100 74
-
-/// 7494 9947
-
-func CountDigitsDivisor(u uint64) (count, div uint64) {
+func CountDigitsDivisor2(u uint64) (count, div uint64) {
 	// 18446744073709551615 <- max amount
-	// could be done in a loop but would most likelly be slower
+	// could be done in a loop but would most likely be slower
 	switch {
-	case u >= 10_000_000_000_000_000_000:
-		return 20, 10_000_000_000
-	case u >= 10_000_000_000_000_000_00:
-		return 19, 1_000_000_000
-	case u >= 10_000_000_000_000_000_0:
-		return 18, 1_000_000_000
-	case u >= 10_000_000_000_000_000:
-		return 17, 100_000_000
-	case u >= 10_000_000_000_000_00:
-		return 16, 100_000_000
-	case u >= 10_000_000_000_000_0:
-		return 15, 10_000_000
-	case u >= 10_000_000_000_000:
-		return 14, 10_000_000
-	case u >= 10_000_000_000_00:
-		return 13, 1_000_000
-	case u >= 10_000_000_000_0:
-		return 12, 1_000_000
-	case u >= 10_000_000_000:
-		return 11, 100_000
-	case u >= 10_000_000_00:
-		return 10, 100_000
-	case u >= 10_000_000_0:
-		return 9, 10_000
-	case u >= 10_000_000:
-		return 8, 10_000
-	case u >= 10_000_00:
-		return 7, 1000
-	case u >= 10_000_0:
-		return 6, 1000
-	case u >= 10_000:
-		return 5, 100
-	case u >= 10_00:
-		return 4, 100
-	case u >= 10_0:
-		return 3, 10
-	case u >= 10:
-		return 2, 10
-	default:
+	case u < 10:
 		return 1, 1
+	case u < 100:
+		return 2, 10
+	case u < 1_000:
+		return 3, 10
+	case u < 10_000:
+		return 4, 100
+	case u < 100_000:
+		return 5, 100
+	case u < 1_000_000:
+		return 6, 1_000
+	case u < 10_000_000:
+		return 7, 1_000
+	case u < 100_000_000:
+		return 8, 10_000
+	case u < 1_000_000_000:
+		return 9, 10_000
+	case u < 10_000_000_000:
+		return 10, 100_000
+	case u < 100_000_000_000:
+		return 11, 100_000
+	case u < 1_000_000_000_000:
+		return 12, 1_000_000
+	case u < 10_000_000_000_000:
+		return 13, 1_000_000
+	case u < 100_000_000_000_000:
+		return 14, 10_000_000
+	case u < 1_000_000_000_000_000:
+		return 15, 10_000_000
+	case u < 10_000_000_000_000_000:
+		return 16, 100_000_000
+	case u < 100_000_000_000_000_000:
+		return 17, 100_000_000
+	case u < 1_000_000_000_000_000_000:
+		return 18, 1_000_000_000
+	case u < 10_000_000_000_000_000_000:
+		return 19, 1_000_000_000
+
+	default:
+		return 20, 10_000_000_000
 	}
-	//18 446 744 073 709 551 615
 }
